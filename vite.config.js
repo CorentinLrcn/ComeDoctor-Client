40cc88495cd0bf6c1aa2af226d53a0ec6f393ad1
CorentinLrcn <corentin.laurencine@ynov.com>
2023-03-27 11:18:45 +0200

    fix: missing manifest

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' }),],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate', manifest: {"name":"comedoctor-client","short_name":"comedoctor-client","start_url":"/","display":"standalone","background_color":"#ffffff","lang":"en","scope":"/"} }),],
 })
